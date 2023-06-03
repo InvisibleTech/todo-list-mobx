a7b3111bb3598cb3613575a42deacbe7f3209a8c
John Ferguson <aninvisiblefriend.tech@gmail.com>
2023-06-03 15:39:17 -0400

    Add support for completed tasks with ratings

--- src/stores/TodoItem.ts
+++ src/stores/TodoItem.ts
@@ -1,24 +1,52 @@
 import { action, makeObservable, observable } from 'mobx';
 
+export type funFactor = undefined | 'yes' | 'no' | 'meh';
+
 export default class TodoItem {
   id = Date.now();
 
   text: string = '';
   isDone: boolean = false;
+  fun: funFactor = undefined;
 
   constructor(text: string) {
     // For a shorter version see makeAutoObservable
     makeObservable(this, {
       text: observable,
       isDone: observable,
+      fun: observable,
       toggleIsDone: action,
       updateText: action,
+      updateFun: action,
+      thatWasFun: action,
+      wouldRatherHaveDoneSomethingElse: action,
+      letsNotDoThatAgain: action,
     });
 
     this.text = text;
   }
 
-  toggleIsDone = () => (this.isDone = !this.isDone);
+  toggleIsDone = () => {
+    this.isDone = !this.isDone;
+  };
+
+  // Making this function an action has benefits: https://mobx.js.org/actions.html
+  thatWasFun = () => {
+    this.toggleIsDone();
+    this.updateFun('yes');
+  };
+
+  wouldRatherHaveDoneSomethingElse = () => {
+    this.toggleIsDone();
+    this.updateFun('meh');
+  };
+
+  letsNotDoThatAgain = () => {
+    this.toggleIsDone();
+    this.updateFun('no');
+  };
 
   updateText = (text: string) => (this.text = text);
+
+  updateFun = (fun: funFactor) => (this.fun = fun);
 }
